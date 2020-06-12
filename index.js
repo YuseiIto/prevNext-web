84ddbfe121a3dd2dd2c015db9242482f567e28ac
Yusei Ito <yuseikun@Outlook.jp>
2020-06-13 03:27:51 +0900

    Print logs

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const path = 'ws://nextprev.yuseiito.com/'
 const sock = new WebSocket(path);
 
+let log = null;
+
+window.onload = function() {
+    log = document.getElementById('log')
+}
+
 sock.addEventListener('open', function(e) {
     console.log('Socket connection suceed');
+    log.innerHTML += "Connection established\n"
 });
 
 function sendNext() {
