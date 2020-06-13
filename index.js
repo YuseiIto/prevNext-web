db8a8c68777454635f03edc831f343a1be357455
Yusei Ito <yuseikun@Outlook.jp>
2020-06-13 23:14:24 +0900

    接続を自分で回復

--- index.js
+++ index.js
@@ -1,60 +1,71 @@
 let log = null;
 let logBtn = null;
-const path = 'wss://server.nextprev.yuseiito.com/'
+const path = "wss://server.nextprev.yuseiito.com/";
 let sock = null;
-
-
+let reConnection = null; // Interval id
 
 function sendNext() {
-    sock.send('next');
+    sock.send("next");
     log.innerHTML += "Next<br/>";
 }
 
 function sendPrev() {
-    sock.send('prev');
+    sock.send("prev");
     log.innerHTML += "Prev<br/>";
 }
 
 function switchLogs() {
-    if (log.style.display == 'none') {
+    if (log.style.display == "none") {
         log.style.display = "inline-block";
-        logBtn.innerText = "-"
+        logBtn.innerText = "-";
     } else {
         log.style.display = "none";
-        logBtn.innerText = "+"
+        logBtn.innerText = "+";
     }
 }
 
-
-window.onload = function() {
-    log = document.getElementById('log');
-    logBtn = document.getElementById('expandLogs');
-
+function connect() {
     try {
         sock = new WebSocket(path);
+        clearInterval(reConnection);
     } catch (e) {
-        log.innerHTML += '<span class="log error">[エラー]</span>WebSocket接続の確立に失敗しました<br/>';
+        log.innerHTML +=
+            '<span class="log error">[エラー]</span>WebSocket接続の確立に失敗しました<br/>';
         log.innerHTML += e;
-        log.innerHTML += "<br/>"
+        log.innerHTML += "<br/>";
     }
+}
 
-    sock.addEventListener('open', function(e) {
-        console.log('Socket connection suceed');
-        log.innerHTML += '<span class="log success">[成功]</span><code>' + path + "</code> へのWebsocket接続を確立しました。<br/>";
+window.onload = function() {
+    log = document.getElementById("log");
+    logBtn = document.getElementById("expandLogs");
 
-        const elements = document.getElementsByClassName('btn');
+    connect();
+
+    sock.addEventListener("open", function(e) {
+        console.log("Socket connection suceed");
+        log.innerHTML +=
+            '<span class="log success">[成功]</span><code>' +
+            path +
+            "</code> へのWebsocket接続を確立しました。<br/>";
+
+        const elements = document.getElementsByClassName("btn");
         for (const e of elements) {
             e.style.color = "hsl(141, 53%, 53%)";
         }
     });
 
-
-    sock.addEventListener('close', function(e) {
-        console.log('Socket disconnected.');
-        log.innerHTML += '<span class="log error">[エラー]</span>接続が切断されました<br/>';
-        const elements = document.getElementsByClassName('btn');
+    sock.addEventListener("close", function(e) {
+        console.log("Socket disconnected.");
+        log.innerHTML +=
+            '<span class="log error">[エラー]</span>接続が切断されました<br/>';
+        const elements = document.getElementsByClassName("btn");
         for (const e of elements) {
             e.style.color = "#555";
         }
+
+        reConnection = setInterval(function() {
+            connect();
+        }, 3000);
     });
-}
+};
