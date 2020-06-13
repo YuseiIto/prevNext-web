6a12af0410992e49b1e021d9d12b34f379c99c91
Yusei Ito <yuseikun@Outlook.jp>
2020-06-13 12:02:23 +0900

    Modify log prints

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-const path = 'ws://server.nextprev.yuseiito.com/'
-const sock = new WebSocket(path);
-
 let log = null;
 let logBtn = null;
+const path = 'ws://server.nextprev.yuseiito.com/'
+let sock = null;
 
-window.onload = function() {
-    log = document.getElementById('log');
-    logBtn = document.getElementById('expandLogs');
-}
 
-sock.addEventListener('open', function(e) {
-    console.log('Socket connection suceed');
-    log.innerHTML += "Connection established to <code>" + path + "</code><br/>";
-});
 
 function sendNext() {
     sock.send('next');
@@ -32,4 +23,28 @@ function switchLogs() {
         log.style.display = "none";
         logBtn.innerText = "+"
     }
+}
+
+
+window.onload = function() {
+    log = document.getElementById('log');
+    logBtn = document.getElementById('expandLogs');
+
+    try {
+        sock = new WebSocket(path);
+    } catch (e) {
+        log.innerHTML += '<span class="log error">[エラー]</span>WebSocket接続の確立に失敗しました<br/>';
+        log.innerHTML += e;
+        log.innerHTML += "<br/>"
+    }
+
+    sock.addEventListener('open', function(e) {
+        console.log('Socket connection suceed');
+        log.innerHTML += '<span class="log success">[成功]</span><code>' + path + "</code> へのWebsocket接続を確立しました。<br/>";
+
+        const elements = document.getElementsByClassName('btn');
+        for (const e of elements) {
+            e.style.color = "hsl(141, 53%, 53%)";
+        }
+    });
 }
