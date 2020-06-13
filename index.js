5be119d8bd9ce9e0b8a652376e770a1b308be41a
Yusei Ito <yuseikun@Outlook.jp>
2020-06-13 11:52:16 +0900

    Modify appearance

--- index.js
+++ index.js
@@ -2,22 +2,34 @@ const path = 'ws://server.nextprev.yuseiito.com/'
 const sock = new WebSocket(path);
 
 let log = null;
+let logBtn = null;
 
 window.onload = function() {
-    log = document.getElementById('log')
+    log = document.getElementById('log');
+    logBtn = document.getElementById('expandLogs');
 }
 
 sock.addEventListener('open', function(e) {
     console.log('Socket connection suceed');
-    log.innerHTML += "Connection established to <code>" + path + "</code><br/>"
+    log.innerHTML += "Connection established to <code>" + path + "</code><br/>";
 });
 
 function sendNext() {
     sock.send('next');
-    log.innerHTML += "Next<br/>"
+    log.innerHTML += "Next<br/>";
 }
 
 function sendPrev() {
     sock.send('prev');
-    log.innerHTML += "Prev<br/>"
+    log.innerHTML += "Prev<br/>";
+}
+
+function switchLogs() {
+    if (log.style.display == 'none') {
+        log.style.display = "inline-block";
+        logBtn.innerText = "-"
+    } else {
+        log.style.display = "none";
+        logBtn.innerText = "+"
+    }
 }
